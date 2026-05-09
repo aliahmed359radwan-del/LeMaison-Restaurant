40b89f60f2b6120fc1c91e0bb4e8205ee03e4fdb
Yassin <yelsebety@gmail.com>
2026-05-09 15:13:55 +0300

    app file edited

--- src/main/java/com/lemaison/leMaison/LeMaisonApplication.java
+++ src/main/java/com/lemaison/leMaison/LeMaisonApplication.java
@@ -1,7 +1,11 @@
 package com.lemaison.leMaison;
 
+import com.lemaison.leMaison.model.MenuItem;
+import com.lemaison.leMaison.repository.MenuItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LeMaisonApplication {
@@ -10,4 +14,17 @@ public class LeMaisonApplication {
         SpringApplication.run(LeMaisonApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner seedMenu(MenuItemRepository menuRepo) {
+        return args -> {
+            if (menuRepo.count() == 0) {
+                menuRepo.save(new MenuItem("French Fries",       "Crispy golden fries",                    50.0));
+                menuRepo.save(new MenuItem("Steak 250g",         "Grilled premium beef steak",             300.0));
+                menuRepo.save(new MenuItem("Ratatouille",        "Classic French vegetable medley",        125.0));
+                menuRepo.save(new MenuItem("White Rice Plate",   "Fluffy white rice with garnish",         100.0));
+                menuRepo.save(new MenuItem("Red Pasta",          "Pasta in rich tomato sauce",             100.0));
+                menuRepo.save(new MenuItem("Pizza Margherita L", "Large margherita with fresh basil",      200.0));
+            }
+        };
+    }
 }
